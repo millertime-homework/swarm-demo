562469ce2c6ced9b9b3f43f112372e7abd10d5ad
Russell <miller.time.baby@gmail.com>
2011-03-07 13:45:13 -0800

    hack hack hack

--- BugAI.java
+++ BugAI.java
@@ -7,6 +7,7 @@
 
 import java.lang.*;
 import java.util.*;
+import java.text.DecimalFormat;
 
 public class BugAI extends AI {
     static Random prng = new Random();
@@ -18,13 +19,23 @@ public class BugAI extends AI {
     public void control(MeView me, AgentView[] agents, ThingView[] things) {
 	if (things != null)
 	    throw new Error("internal error: no things yet");
-	System.out.println(me.a);
-	me.a = 5;
-	double dest = Math.atan(.5-me.y /.5-me.x);
-	if (dest > me.t) 
-	    me.vt -= .3;
-	else if (dest < me.t)
-	    me.vt += .3;
+	me.a = 0;
+	DecimalFormat df = new DecimalFormat("#.000");
+	double dest = Math.atan((.5-me.y) /(.5-me.x)) - Math.PI/4;
+	System.out.println("x: " + df.format(me.x) + " y: " + df.format(me.y));
+	System.out.println("dest: " + df.format(Math.toDegrees(dest)));
+	System.out.println("me: " + df.format(Math.toDegrees(me.t)));
+	if ((Math.abs(dest - me.t) > .1) && (me.vt > 0))
+	    me.vt += .001;
+	else {
+	    me.a = .5;
+	    me.vt = 0;
+	}
+	//System.out.println("x: " + df.format(me.x) + " y: " + df.format(me.y));
+	//System.out.println("dest: " + df.format(dest));
+	//System.out.println("me: " + df.format(me.t));
+	System.out.println("--");
 	//me.vt += Math.PI * nextSignedDouble() * dt;
+	System.exit(0);
     }
 }
